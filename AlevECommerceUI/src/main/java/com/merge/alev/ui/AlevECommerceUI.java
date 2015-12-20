83763d21cfebbd397db84a106066da6ee2d5a2b6
Ahmet OZKESEK <aozkesek@gmail.com>
2015-12-20 14:56:34 +0200

    initial UI project commit

---
+++ AlevECommerceUI/src/main/java/com/merge/alev/ui/AlevECommerceUI.java
@@ -0,0 +1,17 @@
+package com.merge.alev.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlevECommerceUI {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(AlevECommerceUI.class, args);
+		
+		
+		
+	}
+
+}
