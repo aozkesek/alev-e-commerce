4d3c97e5094521f2f8ef54cabc78fd2ed4c06abb
Ahmet OZKESEK <aozkesek@gmail.com>
2015-12-26 21:49:51 +0200

    eclipse project's unimportant things

--- AlevECommerceUI/src/main/java/com/merge/alev/ui/AlevECommerceUI.java
+++ AlevECommerceUI/src/main/java/com/merge/alev/ui/AlevECommerceUI.java
@@ -3,7 +3,7 @@ package com.merge.alev.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="com.merge.alev.ui")
 public class AlevECommerceUI {
 	
 	public static void main(String[] args) {
