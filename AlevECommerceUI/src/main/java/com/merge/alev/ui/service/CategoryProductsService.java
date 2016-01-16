121242c6578c1969f8456e6e399b950ab39bb93c
Ahmet OZKESEK <aozkesek@gmail.com>
2016-01-16 22:44:21 +0200

    home page is now giving an idea except the product's pictures

--- AlevECommerceUI/src/main/java/com/merge/alev/ui/service/CategoryProductsService.java
+++ AlevECommerceUI/src/main/java/com/merge/alev/ui/service/CategoryProductsService.java
@@ -1,5 +1,6 @@
 package com.merge.alev.ui.service;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,13 +8,28 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
+import com.merge.alev.ui.model.Category;
+import com.merge.alev.ui.model.Product;
+import com.merge.alev.ui.model.request.ProductRequest;
+
 @Component("categoryProductsService")
 public class CategoryProductsService implements IPageService {
 
 	@Override
 	public String process(HttpServletRequest request, Model model) {
 		Map<String, Object> modelMap = model.asMap();
+		ProductRequest prequest = new ProductRequest();
+		Product product = new Product();
 		
+		product.setCategory(new Category());
+		product.getCategory().setId((Integer)modelMap.get("categoryId"));
+		
+		prequest.setModel(new ArrayList<Product>());
+		prequest.getModel().add(product);
+		
+		Integer totalRecordNumber = RestProxy.postForObject(ServiceConstants.ProductEndpoint + "/listTotalRecord", prequest, Integer.class);
+		model.addAttribute("responseCode", 0);
+		model.addAttribute("totalRecordNumber", totalRecordNumber);
 		
 		return "products";
 	}
--- AlevECommerceUI/src/main/java/com/merge/alev/ui/service/ProductService.java
+++ AlevECommerceUI/src/main/java/com/merge/alev/ui/service/ProductService.java
@@ -3,8 +3,6 @@ package com.merge.alev.ui.service;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-
 import com.merge.alev.ui.model.Category;
 import com.merge.alev.ui.model.Product;
 import com.merge.alev.ui.model.reponse.ProductResponse;
@@ -13,16 +11,13 @@ import com.merge.alev.ui.model.request.ProductRequest;
 @Component
 public class ProductService {
 	
-	private static final String Version = "1.0.0";
-	private static final String Endpoint = "http://localhost:8080/product";
-	
 	public ProductResponse getProductsByCategory(Category category, int first, int max) {
 		ProductResponse res = new ProductResponse();
 		ProductRequest req = new ProductRequest();
 		
 		req.setFirstRecordNumber(first);
 		req.setMaxRecordNumber(max);
-		req.setVersionNumber(Version);;
+		req.setVersionNumber(ServiceConstants.Version);;
 		req.setModel(new ArrayList<Product>());
 		
 		Product p = new Product();
@@ -30,8 +25,7 @@ public class ProductService {
 		
 		req.getModel().add(p);
 		
-		RestTemplate restTemp = new RestTemplate();
-		res = restTemp.postForObject(Endpoint + "/list", req, ProductResponse.class);
+		res = RestProxy.postForObject(ServiceConstants.ProductEndpoint + "/list", req, ProductResponse.class);
 		
 		return res;
 	}
